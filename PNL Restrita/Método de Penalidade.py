8677e004a9c8a00d378b6e396b1a676684c2047a
fgkcv25 <51954835+fgkcv25@users.noreply.github.com>
2019-10-23 15:55:38 -0300

    método de penalidde ( 3 funçõeS)

--- PNL Restrita/Método de Penalidade.py
+++ PNL Restrita/Método de Penalidade.py
@@ -6,51 +6,78 @@ Created on Wed Oct 23 13:55:51 2019
 """
 
 import numpy as np
-#
+
 #def f(x):
 #    return (1-x[0])**2
-#
 #def gradf(x):
 #    return np.asarray([-2*(1-x[0]),0])
+#def hessf(x):
+#    return np.asarray([[2*x[0],0],[0,0]])
 #
 #def rest(x):
 #    c1 = 10*(x[1]-x[0]**2)
 #    return np.asarray([c1])
-#
 #def gradrest(x):
 #    gradc1 = [-20*x[0], 10]
 #    return np.asarray([gradc1])
+#def hessrest(x):
+#    hessc1 = np.asarray([[-20,0],[0,0]])
+#    return np.asarray([hessc1])
+#n = 2
+#m = 1
+#x = np.asarray([-1.2,1])
+
+
+#def f(x):
+#    return -1
+#def gradf(x):
+#    return np.asarray([0,0])
+#def hessf(x):
+#    return np.asarray([[0,0],[0,0]])
+#def rest(x):
+#    return np.asarray([x[0]**2 + x[1]**2 -25, x[0]*x[1] - 9])
+#def gradrest(x):
+#    return np.asarray([[2*x[0],2*x[1]],[x[1],x[0]]])
+#def hessrest(x):
+#    c1 = np.asarray([[2,0],[0,2]])
+#    c2 = np.asarray([[0,1],[1,0]])
+#    return np.asarray([c1,c2])
+#n = 2
+#m = 2
+#x = np.asarray([2,1])
+
 
 def f(x):
-    return -1
+    return np.log(1+x[0]**2) - x[1]
 def gradf(x):
-    return [0,0]
+    return np.asarray([2*x[0]/(1+x[0]**2),-1])
+def hessf(x):
+    return np.asarray([[-((2*x[0])**2)*(1/(1+x[0]**2)**2) + 2/(1+x[0]**2),0],[0,0]])
+
 def rest(x):
-    return np.asarray([x[0]**2 + x[1]**2 -25, x[0]*x[1] - 9])
+    c1 = (1+x[0]**2)**2 + x[1]**2 - 4
+    return np.asarray([c1])
 def gradrest(x):
-    return np.asarray([[2*x[0],2*x[1]],[x[1],x[0]]])
-
-
-
+    gradc1 = [2*(1+x[0]**2)*2*x[0], 2*x[1]]
+    return np.asarray([gradc1])
+def hessrest(x):
+    hessc1 = np.asarray([[4*(1+x[0]**2) + 4*x[0]*2*x[0],0],[0,2]])
+    return np.asarray([hessc1])
+n = 2
+m = 1
+x = np.asarray([2,2])
 
 
 
 u = 1
-n = 2
-m = 1
-#x = np.asarray([-1.2,1])
-x = np.asarray([2,1])
 t = 1
 y = -u*rest(x)
 
 
 def L(x,y):
-    gradsum = 0
-    for i in range(m):
-        gradsum = y[i]*gradrest(x)[i]
-    return gradf(x) - gradsum
+    return gradf(x) - np.dot(y,gradrest(x))
 
-while np.dot(L(x,y),np.transpose(L(x,y))) > 10**(-4):
+while np.dot(L(x,y),np.transpose(L(x,y))) > 10**(-5):
     def Q(x,u):
         return f(x) + (u/2)*np.dot(rest(x),rest(x))
     def Qu(x):
@@ -60,22 +87,23 @@ while np.dot(L(x,y),np.transpose(L(x,y))) > 10**(-4):
         for i in range(m):
             ci = ci + rest(x)[i]*gradrest(x)[i]
         return gradf(x) + u*ci
-    
     def gradQu(x):
         return gradQ(x,u)
+    def hessQ(x,u):
+        ci = 0
+        for i in range(m):
+            ci = ci + np.outer(gradrest(x)[i],gradrest(x)[i]) + rest(x)[i]*hessrest(x)[i]
+        return hessf(x) + u*ci
+    def hessQu(x):
+        return hessQ(x,u)
+    
     
     a = 1
     c1 = 0.9
-    k = 0
     while np.dot(gradQu(x),gradQu(x)) > t:
-        p = -gradQu(x)
-        if Qu(x+a*p) <= Qu(x) + c1*a*np.dot(gradQu(x),p):
-            x = x + a*p
-            k = k+1
-        else:
-            a = 0.9*a
-        if k >= 500:
-            break
+        p = -np.linalg.solve(hessQu(x),gradQu(x))
+        x = x + a*p
+            
     y = -u*rest(x)
     u = u+1
     t = t/2
